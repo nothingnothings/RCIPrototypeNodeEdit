bc57b1c3c100c64b8f0197ca9f80f20049825bcc
Arthur Panazolo <arthur.panazolo@acad.pucrs.br>
2022-10-17 13:49:40 -0300

    updated banner update logic

--- app.js
+++ app.js
@@ -65,10 +65,13 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
-app.use((req, res, next) => {
+app.post('/banner-image', (req, res, next) => {
   upload(req, res, (error) => {
     if (error instanceof multer.MulterError) {
+      console.log('MULTER ERROR HAS OCCURRED');
     } else if (error) {
+      console.log('AN UNKNOWN ERROR HAS OCCURRED');
+      console.log(error);
     }
   });
   next();
--- controllers/admin.js
+++ controllers/admin.js
@@ -1,7 +1,6 @@
 const fs = require('fs');
 const path = require('path');
 
-
 const { validationResult } = require('express-validator');
 
 exports.getAdminPage = (req, res, _next) => {
@@ -12,7 +11,79 @@ exports.getAdminPage = (req, res, _next) => {
 };
 
 exports.bannerPost = (req, res, next) => {
+  const errors = validationResult(req);
+  const validationErrors = errors.array();
+  console.log(validationErrors);
 
+  const imageData = req.file;
 
+  if (!image) {
+    return res.status(422).render('admin/edit-page', {
+      pageTitle: 'Admin Edit Page',
+      path: 'admin/edit-page',
+      errorMessage: 'O arquivo que foi enviado não é uma imagem.',
+      validationErrors: validationErrors,
+    });
+  }
 
-}
+  if (validationErrors.length > 0) {
+    console.log(validationErrors);
+
+    return res.status(422).render('admin/edit-page', {
+      pageTitle: 'Admin Edit Page',
+      path: 'admin/edit-page',
+      errorMessage: errors.array()[0].msg,
+      validationErrors: validationErrors,
+    });
+  }
+
+  switch (req.pageName) {
+    case 'banner-1':
+      fs.unlink(
+        './images/background-images/Retangulo98.png',
+
+        (err) => {
+          if (err) {
+            console.log(err);
+          }
+
+          fs.writeFile(
+            './images/background-images/Retangulo98.png',
+            imageData,
+
+            (err) => {
+              if (err) {
+                throw err;
+              }
+              console.log('File Saved');
+            }
+          );
+        }
+      );
+    case 'banner-2':
+      fs.unlink(
+        './images/background-images/Retangulo99.png',
+
+        (err) => {
+          console.log(err);
+        }
+      );
+    case 'banner-3':
+      fs.unlink(
+        './images/background-images/Retangulo100.png',
+
+        (err) => {
+          console.log(err);
+        }
+      );
+    case 'banner-4':
+      fs.unlink(
+        './images/background-images/Retangulo101.png',
+
+        (err) => {
+          console.log(err);
+        }
+      );
+    default:
+  }
+};
