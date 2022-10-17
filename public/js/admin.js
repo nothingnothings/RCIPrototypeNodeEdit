50bcea19c79accdc3384dbbc5de36d70d9cfaf56
Arthur Panazolo <arthur.panazolo@acad.pucrs.br>
2022-10-17 16:13:03 -0300

    remade pekeUpload code

--- public/js/admin.js
+++ public/js/admin.js
@@ -4,32 +4,34 @@ $('#file-3').pekeUpload();
 $('#file-4').pekeUpload();
 
 
-function sendForm(payload) {
-  fetch('https://rciexemplo.herokuapp.com/admin/banner-edit', {
-    method: 'POST',
-    body: payload,
-  })
-    .then((res) => {
-      res.json();
-    })
-    .then((data) => {
-      console.log(data);
-    });
-}
 
-var formList = document.querySelectorAll('.needs-validation');
 
-for (form of formList) {
-  form.addEventListener('submit', function (event) {
-    if (form.checkValidity() === false) {
-      event.preventDefault();
-      event.stopPropagation();
-    }
+// function sendForm(payload) {
+//   fetch('https://rciexemplo.herokuapp.com/admin/banner-edit', {
+//     method: 'POST',
+//     body: payload,
+//   })
+//     .then((res) => {
+//       res.json();
+//     })
+//     .then((data) => {
+//       console.log(data);
+//     });
+// }
 
-    form.classList.add('was-validated');
-    const formData = new FormData(form);
-    const payload = new URLSearchParams(formData);
+// var formList = document.querySelectorAll('.needs-validation');
 
-    sendForm(payload);
-  });
-}
+// for (form of formList) {
+//   form.addEventListener('submit', function (event) {
+//     if (form.checkValidity() === false) {
+//       event.preventDefault();
+//       event.stopPropagation();
+//     }
+
+//     form.classList.add('was-validated');
+//     const formData = new FormData(form);
+//     const payload = new URLSearchParams(formData);
+
+//     sendForm(payload);
+//   });
+// }
