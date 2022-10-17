aef89e263f04997c544ff2d55a89ccc9a40e3571
Arthur Panazolo <arthur.panazolo@acad.pucrs.br>
2022-10-16 22:38:09 -0300

    updated edit-page styling

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,14 +1,8 @@
 const { validationResult } = require('express-validator');
 
-
-
-
-
-
 exports.getAdminPage = (req, res, _next) => {
-    res.render('admin/edit-site', {
-        path: '/admin/edit-site',
-        pageTitle: 'Admin Edit Site Page'
-      });
-  
-  }
+  res.render('admin/edit-site', {
+    path: '/admin/edit-site',
+    pageTitle: 'Admin Edit Site Page',
+  });
+};
