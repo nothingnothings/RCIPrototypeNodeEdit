4f48ce33764513acf208e0b72b54602ce8146ddb
Arthur Panazolo <arthur.panazolo@acad.pucrs.br>
2022-10-17 14:37:10 -0300

    updated navbar error page

--- controllers/error.js
+++ controllers/error.js
@@ -1,7 +1,7 @@
 exports.error404 = (req, res, _next) => {
   res.status(404).render('404', {
     pageTitle: 'Página não encontrada.',
-    path: '',
+    path: '/error',
     isLoggedIn: req.session.isLoggedIn
   });
 };
@@ -9,7 +9,7 @@ exports.error404 = (req, res, _next) => {
 exports.error500 = (req, res, _next) => {
   res.status(500).render('500', {
     pageTitle: 'Problema técnico, favor tentar novamente.',
-    path: '',
+    path: '/error',
     isLoggedIn: req.session.isLoggedIn
   });
 };
