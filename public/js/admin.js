bc4af81c663adfb0a5809e3fd1eed56120f980ab
Arthur Panazolo <arthur.panazolo@acad.pucrs.br>
2022-10-17 15:23:27 -0300

    updated form submission logic in edit-page page

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,1 +1,33 @@
-$("#file").pekeUpload();
+$('#file').pekeUpload();
+
+function sendForm(payload) {
+  fetch('https://rciexemplo.herokuapp.com/admin/banner-edit', {
+    method: 'POST',
+    body: payload,
+  })
+    .then((response) => {
+      res.json();
+    })
+    .then((data) => {
+      console.log(data);
+    });
+}
+
+var formList = document.querySelectorAll('.needs-validation');
+
+for (form of formList) {
+  form.addEventListener('submit', function (event) {
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+
+    form.classList.add('was-validated');
+
+    const formData = new FormData(form);
+
+    const payload = new URLSearchParams(formData);
+
+    sendForm(payload);
+  });
+}
