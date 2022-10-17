634941d03105409432f9bde01b33a712a77c11e3
Arthur Panazolo <arthur.panazolo@acad.pucrs.br>
2022-10-17 17:30:55 -0300

    using old code

--- app.js
+++ app.js
@@ -79,6 +79,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.post('/banner-edit', (req, res, next) => {
+
+  console.log(req.body, req.file);
   upload(req, res, (error) => {
     if (error instanceof multer.MulterError) {
       console.log('MULTER ERROR HAS OCCURRED');
