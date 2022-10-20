8161845e2a250dea64a5b9f2348f9b780855d770
Arthur Panazolo <arthur.panazolo@acad.pucrs.br>
2022-10-20 01:40:15 -0300

    attempt to fix modal-show

--- controllers/index.js
+++ controllers/index.js
@@ -9,8 +9,8 @@ exports.getStartingPage = (req, res, _next) => {
 
   return res.render('index/index', {
     path: '/',
-    message: req.flash('message') !== [] ? req.flash('message')[0] : '',
-    errorMessage: req.flash('errorMessage') !== [] ? req.flash('errorMessage')[0] : ''
+    message: req.flash('message')[0] !== '' ? req.flash('message')[0] : '',
+    errorMessage: req.flash('errorMessage')[0] !== '' ? req.flash('errorMessage')[0] : ''
   });
 };
 
