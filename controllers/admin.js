773a39b49ec10b65e9280790dfb18ead408ecec2
Arthur Panazolo <arthur.panazolo@acad.pucrs.br>
2022-10-21 12:59:19 -0300

    attempt to add 'read banners' capability on admin edit page

--- controllers/admin.js
+++ controllers/admin.js
@@ -4,9 +4,36 @@ const path = require('path');
 exports.getAdminPage = (req, res, _next) => {
   // const bannerPageStringArray = [];
 
-  res.render('admin/edit-page', {
-    path: '/admin/edit-page',
-    pageTitle: 'Admin Edit Site Page',
-    csrfToken: req.csrfToken(),
+  const bannerPageStringArray = [];
+
+  fs.readdir('page-text/', (err, folders) => {
+    folders.forEach((folder) => {
+      fs.readdir(`page-text/${folder}/`, (err, file) => {
+        if (err) {
+          console.log(err);
+        }
+        bannerPageStringArray.push(file);
+
+
+      });
+    });
+
+    if (err) {
+      console.log(err);
+      throw err;
+    }
+
+    res.render('admin/edit-page', {
+      path: '/admin/edit-page',
+      pageTitle: 'Admin Edit Site Page',
+      csrfToken: req.csrfToken(),
+    });
+  
   });
+
+  // res.render('admin/edit-page', {
+  //   path: '/admin/edit-page',
+  //   pageTitle: 'Admin Edit Site Page',
+  //   csrfToken: req.csrfToken(),
+  // });
 };
