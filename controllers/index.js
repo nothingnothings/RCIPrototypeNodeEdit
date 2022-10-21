9d52b1c50068bda97bc0eba96d5b835c2036f3a6
Arthur Panazolo <arthur.panazolo@acad.pucrs.br>
2022-10-21 12:44:58 -0300

    added banner read capabilities on all 4 major pages

--- controllers/index.js
+++ controllers/index.js
@@ -20,27 +20,54 @@ exports.getStartingPage = (req, res, _next) => {
 };
 
 exports.getOQueFazemosPage = (req, res, next) => {
-  res.render('index/o-que-fazemos', {
-    path: '/o-que-fazemos',
-    message: req.flash('message')[0],
-    errorMessage: req.flash('errorMessage')[0],
-  });
+  fs.readFile(
+    path.join('page-text' + '/page-2' + '/banner-text.txt'),
+    (err, data) => {
+      if (err) {
+        throw err;
+      }
+
+      res.render('index/o-que-fazemos', {
+        path: '/o-que-fazemos',
+        message: req.flash('message')[0],
+        errorMessage: req.flash('errorMessage')[0],
+      });
+    }
+  );
 };
 
 exports.getQuemSomosPage = (req, res, next) => {
-  res.render('index/quem-somos', {
-    path: '/quem-somos',
-    message: req.flash('message')[0],
-    errorMessage: req.flash('errorMessage')[0],
-  });
+  fs.readFile(
+    path.join('page-text' + '/page-3' + '/banner-text.txt'),
+    (err, data) => {
+      if (err) {
+        throw err;
+      }
+
+      res.render('index/quem-somos', {
+        path: '/quem-somos',
+        message: req.flash('message')[0],
+        errorMessage: req.flash('errorMessage')[0],
+      });
+    }
+  );
 };
 
 exports.getRCInsightsPage = (req, res, next) => {
-  res.render('index/RCInsights', {
-    path: '/RCInsights',
-    message: req.flash('message')[0],
-    errorMessage: req.flash('errorMessage')[0],
-  });
+  fs.readFile(
+    path.join('page-text' + '/page-4' + '/banner-text.txt'),
+    (err, data) => {
+      if (err) {
+        throw err;
+      }
+
+      res.render('index/RCInsights', {
+        path: '/RCInsights',
+        message: req.flash('message')[0],
+        errorMessage: req.flash('errorMessage')[0],
+      });
+    }
+  );
 };
 
 exports.getArtigoPage = (req, res, next) => {
